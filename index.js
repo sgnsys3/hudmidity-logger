3bfcd4cc9e5775cc192425f75ba7256ba6d5c0db
Pureewat Kaewpoy <pureewat.k@gmail.com>
2017-09-22 09:10:16 +0700

    First Init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const winston = require('winston')
+const app = express()
+
+const humidityLogger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({
+      filename: '/var/log/iot/humidity.log',
+      logstash: true,
+    }),
+  ],
+})
+
+app.use(bodyParser.json())
+
+
+app.post('/data', (req, res) => {
+  if(req.body.humidity === undefined || req.body.temperature === undefined) {
+    res.send(JSON.stringify({
+      error: 'Not enough Data',
+    }))
+  }
+  humidityLogger.info(req.body)
+  res.send(JSON.stringify(req.body))
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
